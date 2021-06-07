ec956c83f68a20f21ebb19879d723370e0dc253c
Trevor-akshay <trevorakshay@gmail.com>
2021-06-07 13:06:37 -0700

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.algo;
+
+import com.algo.heap.MaxHeap;
+import com.algo.sortingTechniques.HeapSort;
+import com.algo.tree.BinaryTree;
+import com.algo.tries.Tries_Array;
+import com.algo.tries.Tries_Map;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    /*
+            7
+         4      9
+       3   5  8   10
+     */
+}
+
+
